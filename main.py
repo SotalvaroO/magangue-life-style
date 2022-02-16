4675aec7f178cc78e3248def9e53f4e1585d684b
Santiago Otalvaro Ospina <santiago.otalvaroo@udea.edu.co>
2022-02-15 20:37:35 -0500

    main update an req.txt

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from itertools import product
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
+from app.product.routes import product_routes
 
 app = FastAPI()
+app.include_router(product_routes.route)
 
 @app.get('/')
 async def home():
