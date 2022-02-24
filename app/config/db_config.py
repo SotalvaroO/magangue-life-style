4515da1ff765a74e440ffc6293cec050169ad620
Santiago Otalvaro Ospina <santiago.otalvaroo@udea.edu.co>
2022-02-23 21:55:27 -0500

    cloud db

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -1,7 +1,6 @@
 # MongoDB Driver async
-
 import motor.motor_asyncio
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://<username>:<password>@databasecluster.1ry5d.mongodb.net/test')
 data_base = client.MagangeLifeStyle
 product_collection = data_base.products
