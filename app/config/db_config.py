8600b334d0bbdbe12c96a590d85434a0203f5ae5
Santiago Otalvaro Ospina <santiago.otalvaroo@udea.edu.co>
2022-02-23 22:39:05 -0500

    env

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -1,6 +1,8 @@
 # MongoDB Driver async
+import os
 import motor.motor_asyncio
-
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://<username>:<password>@databasecluster.1ry5d.mongodb.net/test')
+username = os.getenv("MONGO_USER")
+password = os.getenv("MONGO_PASSWORD")
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://{username}:{password}@databasecluster.1ry5d.mongodb.net/test')
 data_base = client.MagangeLifeStyle
 product_collection = data_base.products
