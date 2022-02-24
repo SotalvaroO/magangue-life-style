02ad21d96b65d727beb4e9a6fb0570a8e96a4a30
Santiago Otalvaro Ospina <santiago.otalvaroo@udea.edu.co>
2022-02-23 23:15:08 -0500

    db config

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -1,8 +1,10 @@
 # MongoDB Driver async
 from decouple import config
 import motor.motor_asyncio
+import os
 
-url: str = config('MONGO_URL')
+#url: str = config('MONGO_URL')
+url: str = os.getenv('MONGO_URL')
 client = motor.motor_asyncio.AsyncIOMotorClient(url)
 data_base = client.MagangeLifeStyle
 product_collection = data_base.products
